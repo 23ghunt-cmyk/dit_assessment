d64b1254f1185d2c473f53267f39098a7bb0a4aa
George <23ghunt@wakatipu.school.nz>
2026-04-24 15:00:57 +1200

    Added, reworked and removed items

--- dit_assessment.py
+++ dit_assessment.py
@@ -1,8 +1,9 @@
 # Variables
-error_message = "Please try again"
+error_message = "An error has occurred. Please try again"
+wait_time = 0
 
 # Functions
-def get(prompt):
+def get_num(prompt):
     """other thing"""
     while True:
         try:
@@ -13,28 +14,43 @@ def get(prompt):
 
 def pick_up():
     """thing"""
-    customer_name = input("Name for Pick Up\n> ")
-    customer_phone_number = input("Phone number\n> ")
+    customer_name = input("Name for Pick Up\n> ").lower()
+    customer_phone_number = get_num("Phone number\n> ")
+    order()
 
 def delivery():
      """not thing"""
-     customer_address = input("Address for Dilivery")
-     customer_phone_number = input("Phone number\n> ")
+     delivery = "delivered"
+     customer_address = input("Address for Dilivery\n> ").lower()
+     customer_phone_number = get_num("Phone number\n> ")
+     order()
+
+def order():
+     """ok"""
+     print(pizza)
+     while True:
+          cart = input("Type 'Done' to finish ordering\nEnter order\n> ").lower()
+          if cart == "done":
+               break
+          else:
+               wait_time =+ 5
+
+
 
 # List of pizza options
 pizza = [
-    {"name":"alphabet soup pizza","small": 10,"medium": 20,"large": 30},
-    {"name":"hawian pizza","small": 10,"medium": 20,"large": 30},
-    {"name":"meat lovers pizza","small": 10,"medium": 20,"large": 30},
-    {"name":"margherita pizza","small": 10,"medium": 20,"large": 30},
-    {"name":"pepperoni pizza","small": 10,"medium": 20,"large": 30},
-    {"name":"gravel pizza","small": 10,"medium": 20,"large": 30},
-    {"name":"baked bean pizza","small": 10,"medium": 20,"large": 30},
-    {"name":"sushi","small": 10,"medium n": 20,"large m": 30},
-    {"name":"","small": 10,"medium n": 20,"large m": 30},
-    {"name":"","small": 10,"medium n": 20,"large m": 30},
-    {"name":"","small": 10,"medium n": 20,"large m": 30},
-    {"name":"","small": 10,"medium n": 20,"large m": 30},
+    {"name": "alphabet soup pizza","small": 10,"medium": 20,"large": 30},
+    {"name": "hawian pizza","small": 10,"medium": 20,"large": 30},
+    {"name": "meat lovers pizza","small": 10,"medium": 20,"large": 30},
+    {"name": "margherita pizza","small": 10,"medium": 20,"large": 30},
+    {"name": "pepperoni pizza","small": 10,"medium": 20,"large": 30},
+    {"name": "gravel pizza","small": 10,"medium": 20,"large": 30},
+    {"name": "baked bean pizza","small": 10,"medium": 20,"large": 30},
+    {"name": "sushi","small": 10,"medium": 20,"large": 30},
+    {"name": "mayonnaise","small": 10,"medium n": 20,"large m": 30},
+    {"name": "escalator","small": 10,"medium n": 20,"large m": 30},
+    {"name": "chair","small": 10,"medium n": 20,"large m": 30},
+    {"name": "wood chunk","small": 10,"medium n": 20,"large m": 30},
 ]
 
 # Menu

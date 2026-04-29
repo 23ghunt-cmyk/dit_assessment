e8953a1df115b01502c4e27b67278bd6e3887a1e
George <23ghunt@wakatipu.school.nz>
2026-04-30 11:05:10 +1200

    added and reworked

--- dit_assessment.py
+++ dit_assessment.py
@@ -1,11 +1,11 @@
 """This is a pizza ordering system that allows customers to place pick up or delivery orders."""
 
 # Variables
-error_message = "An error has occurred. Please enter a valid input"
-delivery_fee = 10  # Ask if these should be in all caps or not
-min_per_pizza = 15
+ERROR_MESSAGE = "An error has occurred. Please enter a valid input"
+DELIVERY_FEE = 10
+MIN_PER_PIZZA = 15
 
-# 2D List of pizza options: [Name, Small, Medium, Large]
+# List of pizza options:(Name, Small, Medium, Large)
 pizza = [
     ["alphabet soup pizza", 10, 20, 30],
     ["hawian pizza", 10, 20, 30],
@@ -30,22 +30,22 @@ def get_num(prompt_num):
             num = int(input(prompt_num))
             return num
         except ValueError:
-            print(error_message)
+            print(ERROR_MESSAGE)
 
 
 def pick_up():
     """Gather customer details for a pick up order and start the ordering process."""
     name = input("Name for Pick Up\n> ")
-    phone_number = get_num("Phone number (no spaces)\n> ")
-    order(name, "Pick Up", phone_number)
+    phone_number = get_num("Phone number\n> ")
+    order(name, "pick up", phone_number)
 
 
 def delivery():
     """Gather customer details for a delivery order and start the ordering process."""
     name = input("Name for Delivery\n> ")
     address = input("Address for Delivery\n> ")
-    phone_number = get_num("Phone number (no spaces)\n> ")
-    order(name, "Delivery", phone_number, address)
+    phone_number = get_num("Phone number\n> ")
+    order(name, "delivery", phone_number, address)
 
 
 def order(customer_name, order_type, phone, address="N/A"):
@@ -79,7 +79,7 @@ def order(customer_name, order_type, phone, address="N/A"):
                     price = found_pizza[3]
 
                 total += price
-                wait_time += min_per_pizza
+                wait_time += MIN_PER_PIZZA
                 cart.append(f"{size} {choice}")
                 print(f"Added {size} {choice}. Total: ${total}")
             else:
@@ -91,12 +91,27 @@ def order(customer_name, order_type, phone, address="N/A"):
         print("Cart is empty. Order cancelled")
         return
 
-    print(f"Your total is ${total}")
+    print(f"Your total is ${total + DELIVERY_FEE if order_type == 'delivery' else total}")
 
     # Receipt and confirmation
     while True:
-        break
 
+        confirm = input("Would you like to complete your order? (Y/N)\n> ")
+        if confirm == "y" or confirm == "yes":
+            print(f"Final receipt:\nName: {customer_name}\nType: {order_type}")
+
+            if order_type == "delivery":
+                print(f"Address: {address}")
+
+            print(f"Order: {', '.join(cart)}\nTotal: ${total + DELIVERY_FEE if order_type == 'delivery' else total}\nWait time: {wait_time} mins")
+            break
+
+        elif confirm == "n" or confirm == "no":
+            print("Order successfully canceled.")
+            break
+
+        else:
+            print(ERROR_MESSAGE)
 
 
 # Menu
@@ -111,14 +126,17 @@ while True:
         delivery()
 
     else:
-        print(error_message)
+        print(ERROR_MESSAGE)
 
 # keep new line at end of file
 """
 to add:
-make list print so users can see what they can order
-add .sleep's
-record testing video (remember to test negative cases)
-add .strip() to phone number input
-read comment on line 5
+record testing video (remember to test negative cases) (talk during the video)
+add .strip() to phone number input (not nessasary)
+make the user interface look nicer and .sleep's
+redo varible names
+add ablity to remove items from cart
+change file and / repo name
+change project discription
+change key spesifications
 """
